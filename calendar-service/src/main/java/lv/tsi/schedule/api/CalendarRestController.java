51a5ee0719754b0ca7db73b2cb368e6cc54b1bf1
Edgars Jasmans <yasmans@gmail.com>
2016-05-14 16:26:59 +0300

    Calendar file generation implemented, simple test added.

--- calendar-service/src/main/java/lv/tsi/schedule/api/CalendarRestController.java
+++ calendar-service/src/main/java/lv/tsi/schedule/api/CalendarRestController.java
@@ -1,24 +1,30 @@
 package lv.tsi.schedule.api;
 
 import lv.tsi.schedule.service.CalendarService;
+import net.fortuna.ical4j.data.CalendarOutputter;
 import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 public class CalendarRestController {
 
     private CalendarService calendarService;
+    private CalendarOutputter calendarOutputter = new CalendarOutputter();
 
-    @RequestMapping(value = "/events/{from}/{to}/{lang}", method = RequestMethod.GET, produces = "text/calendar")
-    public Calendar getCalendar(@PathVariable("from") Long from,
-                                @PathVariable("to") Long to,
-                                @PathVariable("lang") String lang,
-                                @RequestParam(value = "teachers", required = false) List<Integer> teachers,
-                                @RequestParam(value = "rooms", required = false) List<Integer> rooms,
-                                @RequestParam(value = "groups", required = false) List<Integer> groups) {
+    @RequestMapping(value = "/calendar/{from}/{to}/{lang}", method = RequestMethod.GET)
+    public void getCalendar(@PathVariable("from") Long from,
+                            @PathVariable("to") Long to,
+                            @PathVariable("lang") String lang,
+                            @RequestParam(value = "teachers", required = false) List<Integer> teachers,
+                            @RequestParam(value = "rooms", required = false) List<Integer> rooms,
+                            @RequestParam(value = "groups", required = false) List<Integer> groups,
+                            HttpServletResponse response) {
 
         /* TODO: validate params
             from and to must be UNIX timestamps
@@ -26,11 +32,24 @@ public class CalendarRestController {
             teachers rooms and groups can consist only from integers
             there must be specified at least one of teachers rooms and groups
         */
-        return calendarService.getCalendar(from, to, lang, teachers, rooms, groups);
+        response.addHeader("Content-disposition", "attachment;filename=calendar.ics");
+        response.setContentType("txt/calendar");
+
+        Calendar calendar = calendarService.getCalendar(from, to, lang, teachers, rooms, groups);
+        try {
+            calendarOutputter.output(calendar, response.getOutputStream());
+            response.flushBuffer();
+        } catch (IOException | ValidationException e) {
+            e.printStackTrace();
+        }
     }
 
     @Autowired
     public void setCalendarService(CalendarService calendarService) {
         this.calendarService = calendarService;
+    }
+
+    public void setCalendarOutputter(CalendarOutputter calendarOutputter) {
+        this.calendarOutputter = calendarOutputter;
     }
 }
--- calendar-service/src/test/java/lv/tsi/schedule/api/CalendarRestControllerTest.java
+++ calendar-service/src/test/java/lv/tsi/schedule/api/CalendarRestControllerTest.java
@@ -1,7 +1,63 @@
 package lv.tsi.schedule.api;
 
-import static org.junit.Assert.*;
+import edu.emory.mathcs.backport.java.util.Arrays;
+import lv.tsi.schedule.Application;
+import lv.tsi.schedule.service.CalendarService;
+import net.fortuna.ical4j.data.CalendarOutputter;
+import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.property.ProdId;
+import net.fortuna.ical4j.model.property.Version;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
 public class CalendarRestControllerTest {
-    //TODO: Implement test
+
+    private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private CalendarRestController calendarRestController;
+
+    private CalendarService calendarService = mock(CalendarService.class);
+    private CalendarOutputter calendarOutputter = new CalendarOutputter(false);
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+        calendarRestController.setCalendarService(calendarService);
+        calendarRestController.setCalendarOutputter(calendarOutputter);
+    }
+
+    @Test
+    public void testGetCalendar() throws Exception {
+        when(calendarService.getCalendar(anyLong(), anyLong(), anyString(), anyListOf(Integer.class), anyListOf(Integer.class),anyListOf(Integer.class)))
+                .thenReturn(new Calendar());
+
+        // Get calendar for a week from 09-05-2016 to 15-05-2016
+        mockMvc.perform(get("/calendar/1462741200000/1463346000000/lv?teachers=11292,8315,18211"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.parseMediaType("txt/calendar")));
+
+    }
 }
